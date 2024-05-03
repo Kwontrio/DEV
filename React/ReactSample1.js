35db974f65e9c4dd55985c73ae36819a1d780db5
Kwontrio <kwontrio@naver.com>
2024-05-03 15:52:33 +0900

    react helloworld

--- React/ReactSample1.js
+++ React/ReactSample1.js
@@ -1,2 +1,10 @@
-ReactSample1.js
-23
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<Hello />);
